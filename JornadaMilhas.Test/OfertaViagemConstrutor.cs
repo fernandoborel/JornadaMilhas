be375a4f8cd2e30f51df0d47f001cf00acf80ae7
Fernando Borel <fernandoborel@fiotec.fiocruz.br>
2025-10-09 15:55:21 -0300

    test: Theory implementado nos métodos de validação de oferta e preço.

---
+++ JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -0,0 +1,57 @@
+﻿using JornadaMilhas.Modelos;
+
+namespace JornadaMilhas.Test;
+
+public class OfertaViagemConstrutor
+{
+    [Theory]
+    [InlineData("", null, "2025-10-07", "2025-10-15", 0, false)]
+    [InlineData("OrigemTeste", "DestinoTeste", "2025-10-07", "2025-10-15", 100, true)]
+    [InlineData(null, "Rio de Janeiro", "2025-10-07", "2025-10-15", -1, false)]
+    [InlineData("Vitória", "São Paulo", "2025-12-01", "2025-12-10", 680, true)]
+    [InlineData("Rio de Janeiro", "São Paulo", "2025-12-01", "2025-12-10", 680, true)]
+    public void RetornaEhValidoDeAcordoComDadosDeEntrada(string origem, string destino, string dataIda, string dataVolta, double preco, bool validacao)
+    {
+        //cenário - arrange
+        Rota rota = new Rota(origem, destino);
+        Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
+
+        //ação - act
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //verificação - assert
+        Assert.Equal(validacao, oferta.EhValido);
+    }
+
+    [Fact]
+    public void RetornaMensagemDeErroDeRotaOuPeriodoInvalidosQuandoRotaNula()
+    {
+        Rota rota = null;
+        Periodo periodo = new Periodo(new DateTime(2025, 10, 07), new DateTime(2025, 10, 15));
+        double preco = 100.0;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
+    }
+
+    [Theory]
+    [InlineData("", null, "2025-10-07", "2025-10-15", 0, false)]
+    [InlineData("OrigemTeste", "DestinoTeste", "2025-10-07", "2025-10-15", 100, true)]
+    [InlineData(null, "Rio de Janeiro", "2025-10-07", "2025-10-15", -100, false)]
+    [InlineData("Vitória", "São Paulo", "2025-12-01", "2025-12-10", 680, true)]
+    [InlineData("Rio de Janeiro", "São Paulo", "2025-12-01", "2025-12-10", 500, true)]
+    public void RetornaMensagemDeErroDePrecoInvalidoQuandoPrecoMenorQueZero(string origem, string destino, string dataIda, string dataVolta, double preco, bool validacao)
+    {
+        //arrange
+        Rota rota = new Rota(origem, destino);
+        Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
+
+        //act
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //assert
+        Assert.Equal(validacao, oferta.EhValido);
+    }
+}
---
+++ JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,42 @@
+﻿using JornadaMilhas.Modelos;
+
+namespace JornadaMilhas.Test;
+
+public class OfertaViagemDesconto
+{
+    [Fact]
+    public void RetornaPrecoAtualizadoQuandoAplicadoDesconto()
+    {
+        //arrange
+        Rota rota = new Rota("OrigemA", "DestinoB");
+        Periodo periodo = new Periodo(new DateTime(2025, 10, 07), new DateTime(2025, 10, 15));
+        double precoOriginal = 100.0;
+        double desconto = 20.00; // 20%
+        double precoComDesconto = precoOriginal - desconto;
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+        //act
+        oferta.Desconto = desconto;
+
+        //assert
+        Assert.Equal(precoComDesconto, oferta.Preco);
+    }
+
+    [Fact]
+    public void RetornaDescontoMaximoQuandoValorDescontoMaiorQuePreco()
+    {
+        //arrange
+        Rota rota = new Rota("OrigemA", "DestinoB");
+        Periodo periodo = new Periodo(new DateTime(2025, 10, 07), new DateTime(2025, 10, 15));
+        double precoOriginal = 100.0;
+        double desconto = 120.00;
+        double precoComDesconto = 30.00;
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+        //act
+        oferta.Desconto = desconto;
+
+        //assert
+        Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+    }
+}
